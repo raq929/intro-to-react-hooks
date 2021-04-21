527b05ea14fa5dffa17aa0666f8a0f1220de9f52
Lael Birch <lbirch@edx.org>
2021-04-21 09:24:39 -0400

    Remove parenthetical
    No longer relevant

--- src/UsefulLinks.jsx
+++ src/UsefulLinks.jsx
@@ -5,7 +5,7 @@ const UsefulLinks = () => {
     <>
     <h2>Hooks references</h2>
     <ul>
-      <li><a href="https://reactjs.org/docs/hooks-reference.html">React hooks reference</a> (contains cheats for the first example)</li>
+      <li><a href="https://reactjs.org/docs/hooks-reference.html">React hooks reference</a></li>
       <li><a href="https://reactjs.org/docs/hooks-faq.html#from-classes-to-hooks">Comparing class lifecycle to hook lifecycle</a></li>
       <li><a href="https://the-guild.dev/blog/react-hooks-system">Deep dive under the hood of React hooks</a></li>
     </ul>
